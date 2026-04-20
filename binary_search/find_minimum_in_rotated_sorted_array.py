08d3c8b010c9508688abc4df4b2e247b8539343d
virinchi <rangerdelli@gmail.com>
2026-04-20 15:16:46 +0530

    add binary search probs

---
+++ binary_search/find_minimum_in_rotated_sorted_array.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findMin(self, nums):
+
+        low, high = 0, len(nums) - 1
+
+        while low < high:
+            mid = (low + high) // 2
+
+            if nums[mid] > nums[high]:
+                low = mid + 1
+            else:
+                high = mid
+
+        return nums[low]
+
+sol = Solution()
+print(sol.findMin(nums = [3]))
