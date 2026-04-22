d45afd8821cb9bd727a8302cb6189cfe38c62d19
virinchi <rangerdelli@gmail.com>
2026-04-22 14:37:20 +0530

    add linked list impl

---
+++ linkedlists/linkedlist_implementation.py
@@ -0,0 +1,163 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+    def __repr__(self):
+        return str(self.__dict__)
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+
+    def __repr__(self):
+        return str(self.__dict__)
+
+    def __str__(self):
+        return " -> ".join(str(x) for x in self) + " -> None"
+
+    def __len__(self):
+        return self.length
+
+    def __iter__(self):
+        curr = self.head
+        while curr:
+            yield curr.data
+            curr = curr.next
+
+    def is_empty(self):
+        return self.length == 0
+
+    def prepend(self, data):
+        new_node = Node(data)
+
+        if self.is_empty():
+            self.head = self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+
+        self.length += 1
+
+    def append(self, data):
+        new_node = Node(data)
+
+        if self.is_empty():
+            self.head = self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+
+        self.length += 1
+
+    def insert(self, index, data):
+        if index <= 0:
+            self.prepend(data)
+            return
+        if index >= self.length:
+            self.append(data)
+            return
+
+        new_node = Node(data)
+        curr = self.head
+
+        for _ in range(index - 1):
+            curr = curr.next
+
+        new_node.next = curr.next
+        curr.next = new_node
+        self.length += 1
+
+
+    def delete_by_index(self, index):
+        if index < 0 or index >= self.length:
+            raise IndexError("Index out of range")
+
+        if index == 0:
+            self.head = self.head.next
+            if self.length == 1:
+                self.tail = None
+            self.length -= 1
+            return
+
+        curr = self.head
+        for _ in range(index - 1):
+            curr = curr.next
+
+        to_delete = curr.next
+        curr.next = to_delete.next
+
+        if to_delete == self.tail:
+            self.tail = curr
+
+        self.length -= 1
+
+    def delete_by_value(self, data):
+        if self.is_empty():
+            raise ValueError("List is empty")
+
+        curr = self.head
+        prev = None
+
+        while curr:
+            if curr.data == data:
+                if prev is None:
+                    self.head = curr.next
+                    if self.head is None:
+                        self.tail = None
+                else:
+                    prev.next = curr.next
+                    if curr == self.tail:
+                        self.tail = prev
+
+                self.length -= 1
+                return
+
+            prev = curr
+            curr = curr.next
+
+        raise ValueError("Value not found")
+
+    def find(self, data):
+        curr = self.head
+        index = 0
+
+        while curr:
+            if curr.data == data:
+                return index
+            curr = curr.next
+            index += 1
+
+        return -1
+
+    def get(self, index):
+        if index < 0 or index >= self.length:
+            raise IndexError("Index out of range")
+
+        curr = self.head
+        for _ in range(index):
+            curr = curr.next
+
+        return curr.data
+
+    def clear(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+
+
+myLinkedList = LinkedList()
+print(myLinkedList)
+myLinkedList.prepend(3)
+myLinkedList.prepend(4)
+myLinkedList.append(5)
+print(myLinkedList)
+myLinkedList.insert(3,6)
+myLinkedList.delete_by_value(3)
+print(myLinkedList)
+myLinkedList.prepend(3)
+myLinkedList.delete_by_index(3)
+print(myLinkedList)
