4694f176a9b28a5b4904ad7d70389f1489961af8
virinchi <rangerdelli@gmail.com>
2026-04-28 13:04:38 +0530

    add merge k sorted lists heap

---
+++ heaps/merge-k-sorted-lists.py
@@ -0,0 +1,30 @@
+import heapq
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def mergeKLists(self, lists):
+        
+        heap = []
+        for i, node in enumerate(lists):
+            if node:
+                heapq.heappush(heap, (node.val, i, node))
+        
+        dummy = ListNode(-1)
+        curr = dummy
+        
+        while heap:
+            val, i, node = heapq.heappop(heap)
+            
+            curr.next = node
+            curr = curr.next
+            
+            if node.next:
+                heapq.heappush(heap, (node.next.val, i, node.next))
+        
+        return dummy.next
+
+sol = Solution()
+print(sol.mergeKLists(lists = [[1,4,5],[1,3,4],[2,6]]))
